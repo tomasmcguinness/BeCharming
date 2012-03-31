56d039e2010fe0b4d03576b445bea0f6d6d3fb77
unknown <Tomas@ThinkPadWin8.(none)>
2012-03-31 12:48:28 +0100

    Work on the app

---
+++ BeCharming.Metro/ViewModels/ShareTargetsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeCharming.Metro.ViewModels
+{
+    public class ShareTargetsViewModel
+    {
+        public ShareTargetsViewModel()
+        {
+            Targets = new ObservableCollection<ShareTarget>();
+        }
+
+        public ObservableCollection<ShareTarget> Targets { get; private set; }
+
+        public void LoadTargets()
+        {
+            ShareTarget t = new ShareTarget();
+            t.Name = "Test";
+            Targets.Add(t);
+        }
+    }
+}
