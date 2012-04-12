74fad31b06047e4aadc3d627dfda09d68dc763c2
= <=>
2012-04-12 09:01:46 +0100

    Got the variableSizedGridView working.

---
+++ BeCharming.Metro/VariableGridView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeCharming.Metro.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace BeCharming.Metro
+{
+    public class VariableGridView : GridView
+    {
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            var viewModel = item as ShareTarget;
+
+            element.SetValue(VariableSizedWrapGrid.ColumnSpanProperty, viewModel.Width);
+            element.SetValue(VariableSizedWrapGrid.RowSpanProperty, viewModel.Height);
+            element.SetValue(HorizontalContentAlignmentProperty, HorizontalAlignment.Stretch);
+            element.SetValue(VerticalContentAlignmentProperty, VerticalAlignment.Stretch);
+
+            base.PrepareContainerForItemOverride(element, item);
+        }
+    }
+}
